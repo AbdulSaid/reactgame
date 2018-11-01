a4ab43cf5acba53e75e1fa662120f28703587914
Abdul Said <abdulrahman.asaid@gmail.com>
2018-11-01 09:32:32 -0400

    Updates

---
+++ precache-manifest.6f705a479c8705c86feb10517d99d578.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d093d11a6420a224d347",
+    "url": "/reactgame/static/js/runtime~main.d093d11a.js"
+  },
+  {
+    "revision": "1a4db8b2488e185c5b18",
+    "url": "/reactgame/static/js/main.1a4db8b2.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/reactgame/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "1a4db8b2488e185c5b18",
+    "url": "/reactgame/static/css/main.44eb23de.chunk.css"
+  },
+  {
+    "revision": "924e60fc7b2d8d6f1bbee34758ddba9e",
+    "url": "/reactgame/index.html"
+  }
+];
