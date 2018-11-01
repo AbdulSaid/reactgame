7b08f48a75b7b30d93e1c3e541c5f33912082552
Abdul Said <abdulrahman.asaid@gmail.com>
2018-11-01 09:01:45 -0400

    add components

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import FriendCard from './components/FriendCard';
+import Navbar from './components/Navbar';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
+import Container from './Container';
+import Row from './Row.js';
+import Column from './Column';
+import friends from './friends.json';
+import './App.css';
+
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: '',
+    clicked: []
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ''
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } else if (newScore === 12) {
+      this.setState({ rightWrong: 'You Win!' });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: 'NBA',
+      clicked: []
+    });
+    this.handleShuffle();
+    alert('You Lose, Try Again');
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          title="NBA Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+        <Title>
+          Try to Click on Each NBA Player. Don't click the same player twice...
+          or you Lose!
+        </Title>
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
